8b2b61a09724baa98c6a9af0594cacfe51e914a2
Dev Raj <devraj00260.24@kgpian.iitkgp.ac.in>
2025-06-26 13:10:12 +0530

    Documented the gang_signs.py file

--- gang_signs.py
+++ gang_signs.py
@@ -11,19 +11,20 @@ import pyautogui as pg
 
 
 
-cap = cv2.VideoCapture(0)                                                                    
-hand_detector = htm.HandDetector()
-folder_path = "gang_signs_overlays"
-last_trigger_time = 0
-cooldown = 3
-typing_state = False
+cap = cv2.VideoCapture(0)                           #Getting the camera ready                                               
+hand_detector = htm.HandDetector()                  #Instancing the HandDetector class
+folder_path = "gang_signs_overlays"                 #Folder containing signs' overlays
+last_trigger_time = 0                               #To store last time a gesture was registered, for cool-down purposes
+cooldown = 3                                        #Seconds for which the computer will not respond to gestures after a gesture is registered
+typing_state = False                                #Flag to toggle typing mode on/off
 
-def type_emoji(emoji):
+#Function to type the emoji according to the gesture
+def type_emoji(emoji):                              
     pyperclip.copy(emoji)
     pg.hotkey("ctrl", "v")
     #pg.press("enter")
 
-# Load all overlay PNGs (with resizing)
+#Load all overlay PNGs (with resizing)
 def load_overlay_images(folder_path, size=(100, 100)):
     overlays = {}
     for file_name in os.listdir(folder_path):
@@ -40,16 +41,18 @@ def load_overlay_images(folder_path, size=(100, 100)):
                 print(f"Warning: Could not read {full_path}")
     return overlays
 
-
+#Function to calculate distance between two landmarks, used in gestures like the 'ok' gesture
 def distance(pt1, pt2):
     return math.hypot(pt1[0] - pt2[0], pt1[1] - pt2[1])
 
+#Function to detect the shown hand is left or right, crucial when gestures are horizontally inclined
 def left_or_right(fingers):
     wrist = fingers[0][1]
     pinky = fingers[20][1]
 
     return "Left" if wrist < pinky else "Right"
 
+#Function to check inclination of hand, horizontal or vertical
 def check_horizontal(lmList):
     x1, y1 = lmList[0][1], lmList[0][2]   # Wrist
     x2, y2 = lmList[9][1], lmList[9][2]   # Middle MCP
@@ -59,7 +62,7 @@ def check_horizontal(lmList):
     return -50 < angle_deg < 50 or -179 <= angle_deg < -130 or int(angle_deg) == 180 or 130 < angle_deg <= 179
     
 
-# Define detection logic for 'yo' sign
+#Detection logic for 'yo' sign
 def yo_sign(handsList):
     fingers = handsList[0]['lmList']
     index_tip = fingers[8][2]
@@ -74,11 +77,12 @@ def yo_sign(handsList):
 
     return (not check_horizontal(fingers) and index_tip < index_mcp and middle_tip > middle_mcp and ring_tip > ring_mcp and pinky_tip < pinky_mcp)
 
-
+#Detection logic for 'fist' sign
 def fist_sign(handsList):
     fingers = handsList[0]['lmList']
     return (all(fingers[i][2] > fingers[i-2][2] for i in [8, 12, 16, 20]) or all(fingers[i][2] < fingers[i+1][2] for i in [5, 9, 13, 17])) and not check_horizontal(fingers)
 
+#Detection logic for 'peace' sign
 def peace_sign(handsList):
     fingers = handsList[0]['lmList']
     return (
@@ -89,11 +93,12 @@ def peace_sign(handsList):
         fingers[20][2] > fingers[18][2]      
     )
 
+#Detection logic for 'hello' or 'waving' sign
 def hello_sign(handsList):
     fingers = handsList[0]['lmList']
     return all(fingers[i][2] < fingers[i-2][2] for i in [4, 8, 12, 16, 20]) and not check_horizontal(fingers)
 
-
+#Detection logic for 'pointing up' sign
 def point_up_sign(handsList):
     fingers = handsList[0]['lmList']
     return (
@@ -104,6 +109,8 @@ def point_up_sign(handsList):
         fingers[20][2] > fingers[18][2]       # Pinky down
     )
 
+#Detection logic for middle finger sign. Commented out due to profanity issues. Comments may be removed in the future if it doesn't create problems.
+'''
 def middle_finger_sign(handsList):
     fingers = handsList[0]['lmList']
     return (
@@ -114,6 +121,9 @@ def middle_finger_sign(handsList):
         fingers[20][2] > fingers[18][2]
     )
 
+'''
+
+#Detection logic for 'ok' sign
 def ok_sign(handsList):
     fingers = handsList[0]['lmList']
 
@@ -121,7 +131,7 @@ def ok_sign(handsList):
     index_tip = fingers[8]
 
     # Thumb and index touching
-    thumb_index_touch = distance(thumb_tip, index_tip) < 25
+    thumb_index_touch = distance(thumb_tip, index_tip) < 25         #Here the distance() function is taking action
 
     # Middle, ring, pinky up
     middle_up = fingers[12][2] < fingers[10][2]
@@ -130,6 +140,8 @@ def ok_sign(handsList):
 
     return thumb_index_touch and middle_up and ring_up and pinky_up
 
+
+#Detection logic for 'fingers crossed' sign
 def fingers_crossed_sign(handsList):
     fingers = handsList[0]['lmList']
     return  (
@@ -141,6 +153,8 @@ def fingers_crossed_sign(handsList):
         abs(fingers[8][1] - fingers[12][1]) < 10
     )
 
+
+#Detection logic for 'thumbs up' sign
 def thumbs_up_sign(handsList):
     fingers = handsList[0]['lmList']
     horizontal = check_horizontal(fingers)  
@@ -167,6 +181,8 @@ def thumbs_up_sign(handsList):
 
     return ans
 
+
+#Detection logic for 'gun' sign
 def gun_sign(handsList):
     fingers = handsList[0]['lmList']
     horizontal = check_horizontal(fingers)
@@ -191,6 +207,7 @@ def gun_sign(handsList):
 
     return ans
 
+#Detection logic for 'call' sign
 def call_sign(handsList):
     fingers = handsList[0]['lmList']
     horizontal = check_horizontal(fingers)
@@ -217,6 +234,7 @@ def call_sign(handsList):
 
     return ans
 
+#Detection logic for 'thumbs down' sign
 def thumbs_down_sign(handsList):
     fingers = handsList[0]['lmList']
     horizontal = check_horizontal(fingers)
@@ -243,12 +261,14 @@ def thumbs_down_sign(handsList):
 
     return ans
 
+''' <--- All emojis to be typed according to their respective gestures ---> '''
+
 emoji_yo_sign = "🤘"              # Rock on / Yo!
 emoji_fist_sign = "✊"            # Fist
 emoji_peace_sign = "✌️"           # Peace
 emoji_hello_sign = "👋"           # Open palm
 emoji_point_up_sign = "👆"        # Pointing up
-emoji_middle_finger_sign = "🖕"   # Middle finger (use responsibly)
+#emoji_middle_finger_sign = "🖕"   # Middle finger (use when profanity isn't an issue)
 emoji_ok_sign = "👌"              # OK gesture
 emoji_fingers_crossed_sign = "🤞" # Fingers crossed
 emoji_thumbs_up_sign = "👍"       # Thumbs up
@@ -258,8 +278,11 @@ emoji_thumbs_down_sign = "👎"     # Thumbs down
 
 
 
-overlays = load_overlay_images(folder_path)
-curr_overlay = None
+overlays = load_overlay_images(folder_path)                 #Dictionary with keys as sign name and values as their respective overlay images as locations
+curr_overlay = None                                         #Current overlay being displayed on the top left of the screen
+
+
+''' <--- The working loop starts ---> '''
 
 while True:
     success, img = cap.read()
@@ -270,13 +293,15 @@ while True:
     img = cv2.flip(img, 1)
     img = hand_detector.findHands(img, True, False)
     handsList = hand_detector.findPosition(img)
-    current_time = time.time()
+    current_time = time.time()                             
 
     if typing_state:
 
-        cv2.putText(img, "Typing active! Press 't' to toggle", (10, 450), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        cv2.putText(img, "Typing active! Press 'T' to toggle", (10, 450), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
         if handsList:
+
+            #if-elif chain for all gestures
 
             if yo_sign(handsList) and current_time - last_trigger_time > cooldown:
                 type_emoji(emoji_yo_sign)
@@ -313,10 +338,12 @@ while True:
                 curr_overlay = overlays['point_up_sign']
                 last_trigger_time = current_time
 
-            elif middle_finger_sign(handsList) and current_time - last_trigger_time > cooldown:
-                type_emoji(emoji_middle_finger_sign)
-                curr_overlay = overlays['middle_finger_sign']
-                last_trigger_time = current_time
+            # <--- Commented out due to profanity issues --->
+            #elif middle_finger_sign(handsList) and current_time - last_trigger_time > cooldown:
+                #type_emoji(emoji_middle_finger_sign)
+                #curr_overlay = overlays['middle_finger_sign']
+                #last_trigger_time = current_time
+            
 
             elif ok_sign(handsList) and current_time - last_trigger_time > cooldown:
                 type_emoji(emoji_ok_sign)
@@ -340,16 +367,18 @@ while True:
 
 
     else:
-        cv2.putText(img, "Typing inactive. Press 't' to toggle", (10, 450), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        cv2.putText(img, "Typing inactive. Press 'T' to toggle", (10, 450), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
-    if curr_overlay is not None:
+    if curr_overlay is not None:                                #Display the overlay
         img[0:100, 0:100] = curr_overlay
 
-    # Draw mode indicator rectangle (top-right corner)
+    # Draw mode indicator rectangle for typing (green) /non-typing (red) state(top-right corner)
     color = (0, 255, 0) if typing_state else (0, 0, 255)
     cv2.rectangle(img, (540, 10), (630, 50), color, -1)
     cv2.putText(img, "TYP", (545, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,255,255), 2)
 
+    #Draw mode indicator rectangle for cooldown active (yellow) / inactive (blue) state (top-right corner, left to the typing state indicator)
+    #When active, gesture will not be read to avoid spamming
     if typing_state :
         cooldown_over = (current_time - last_trigger_time) > cooldown
         cool_color = (255, 0, 0) if cooldown_over else (0, 255, 255)
@@ -363,7 +392,7 @@ while True:
     if key == ord('t'):
         typing_state = not typing_state
 
-    elif key == ord('q'):
+    elif key == ord('q'):                               #Exit the program
         break
 
 cap.release()
